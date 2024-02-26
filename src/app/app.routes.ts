1f2fd5eb53f7ae14a371c2c8d66c0e5fe59b2ff8
Carloslt <carloslt.proyectos@gmail.com>
2024-02-26 15:00:03 +0100

    ✅ component: add navbar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,4 +4,5 @@ import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'library', component: HomeComponent },
 ];
