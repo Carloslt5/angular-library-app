776ae3d42b605a6e2f4efa415fb493d725ad7b2e
Carloslt <carloslt.proyectos@gmail.com>
2024-02-28 11:35:53 +0100

    🔧 style: add main container on home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MainContainerComponent } from '../../components/layout/main-container/main-container.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, MainContainerComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
