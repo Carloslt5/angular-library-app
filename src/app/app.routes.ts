b418de62953f6710cca3b71924c62fea72950a7b
Carloslt <carloslt.proyectos@gmail.com>
2024-02-29 15:18:52 +0100

    ✅ feat: create details route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LibraryComponent } from './pages/library/library.component';
+import { BookDetailsComponent } from './pages/book-details/book-details.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'library', pathMatch: 'full', component: LibraryComponent },
+  { path: 'library/:id', pathMatch: 'full', component: BookDetailsComponent },
 ];
---
+++ src/app/pages/book-details/book-details.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'book-details-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-details.component.html',
+})
+export class BookDetailsComponent {}
