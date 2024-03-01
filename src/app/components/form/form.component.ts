d8e2d4bea5cca33fefd5d0ce1f003fb74e3483d8
Carloslt <carloslt.proyectos@gmail.com>
2024-03-01 15:02:26 +0100

    ✅ task: add form on create and styles

---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'form-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './form.component.html',
+})
+export class FormComponent {}
--- src/app/pages/createbook/createbook.component.ts
+++ src/app/pages/createbook/createbook.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MainContainerComponent } from '../../components/layout/main-container/main-container.component';
+import { FormComponent } from '../../components/form/form.component';
 
 @Component({
   selector: 'create-page',
   standalone: true,
-  imports: [MainContainerComponent],
+  imports: [MainContainerComponent, FormComponent],
   templateUrl: './createbook.component.html',
 })
 export class CreatebookComponent {}
