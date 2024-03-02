427692437dc3e104c52db176dd9186634e3e0eaa
Carloslt <carloslt.proyectos@gmail.com>
2024-03-02 19:32:56 +0100

    🔧 feat: add image on create form and dinamic loading

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { UploadService } from '../../core/services/upload.service';
+import { AsyncPipe } from '@angular/common';
+export interface UploadResponse {
+  publicUrl: string;
+}
 
 @Component({
   selector: 'form-component',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, ReactiveFormsModule],
   templateUrl: './form.component.html',
 })
-export class FormComponent {}
+export class FormComponent implements OnInit {
+  bookData = new FormGroup({
+    title: new FormControl(''),
+    author: new FormControl(''),
+    link: new FormControl(''),
+    categorie: new FormControl(''),
+    imageURL: new FormControl({
+      publicUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL3DlrxkyG-D62QsmOFHoe_pCaaIIVWnItM6NMqtYqTsXNIpdB',
+    }),
+    year: new FormControl(null),
+  });
+
+  constructor(private fileServices: UploadService) {}
+
+  ngOnInit(): void {
+    console.log('-------------', this.bookData.value);
+  }
+
+  uploadImg(event: Event) {
+    const fileInput = event.target as HTMLInputElement;
+    const imagenData = fileInput.files ? fileInput.files[0] : null;
+    if (imagenData) {
+      const formData = new FormData();
+      formData.append('imageData', imagenData);
+      this.fileServices.uploadImage(formData).subscribe((response) => {
+        this.bookData.patchValue({
+          imageURL: { ...response },
+        });
+      });
+    }
+  }
+
+  sendForm(event: Event) {
+    event.preventDefault();
+    console.log('Formulario enviado:', this.bookData.value);
+  }
+}
---
+++ src/app/core/services/upload.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UploadResponse } from '../../components/form/form.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UploadService {
+  constructor(private http: HttpClient) {}
+
+  uploadImage(imageData: FormData) {
+    return this.http.post<UploadResponse>(
+      'http://localhost:5005/api/upload',
+      imageData
+    );
+  }
+}
