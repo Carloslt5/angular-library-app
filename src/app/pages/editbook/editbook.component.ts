766825f30970a56907a2f583f99a2fb924ffe9f4
Carloslt <carloslt.proyectos@gmail.com>
2024-03-04 16:27:13 +0100

    ✅ task: pass book detail to edit page

--- src/app/pages/editbook/editbook.component.ts
+++ src/app/pages/editbook/editbook.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MainContainerComponent } from '../../components/layout/main-container/main-container.component';
+import { FormComponent } from '../../components/form/form.component';
+import { Book } from '../../interface/book';
 
 @Component({
   selector: 'app-editbook',
   standalone: true,
-  imports: [],
+  imports: [MainContainerComponent, FormComponent],
   templateUrl: './editbook.component.html',
-  styleUrl: './editbook.component.css'
+  styleUrl: './editbook.component.css',
 })
-export class EditbookComponent {
+export class EditbookComponent implements OnInit {
+  bookDetails!: Book;
 
+  constructor() {}
+
+  ngOnInit() {
+    this.bookDetails = history.state.book;
+  }
 }
