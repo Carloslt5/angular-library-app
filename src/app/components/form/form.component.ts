b013411b0348f3bec03110251ccd01eed807fbd7
Carloslt <carloslt.proyectos@gmail.com>
2024-03-04 15:34:53 +0100

    ✅ refactor: create and form, sendservices

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,10 +1,9 @@
 import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { BOOK_CATEGORY_ARRAY } from '../../../constants/categories.constants';
-import { BookService } from '../../core/services/book.service';
-import { UploadService } from '../../core/services/upload.service';
 import { Book } from '../../interface/book';
+import { UploadService } from '../../core/services/upload.service';
 
 @Component({
   selector: 'form-component',
@@ -16,20 +15,18 @@ export class FormComponent {
   bookData = new FormGroup({
     title: new FormControl(''),
     author: new FormControl(''),
-    link: new FormControl(''),
+    link: new FormControl('https://www.google.com'),
     categories: new FormControl(''),
     imageURL: new FormControl(
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL3DlrxkyG-D62QsmOFHoe_pCaaIIVWnItM6NMqtYqTsXNIpdB'
     ),
     year: new FormControl(''),
   });
-
   bookCategories = BOOK_CATEGORY_ARRAY;
 
-  constructor(
-    private fileService: UploadService,
-    private bookService: BookService
-  ) {}
+  @Output() formSubmit = new EventEmitter<Partial<Book>>();
+
+  constructor(private fileService: UploadService) {}
 
   uploadImg(event: Event) {
     const fileInput = event.target as HTMLInputElement;
@@ -47,10 +44,6 @@ export class FormComponent {
 
   sendForm(event: Event) {
     event.preventDefault();
-    this.bookService
-      .create(this.bookData.value as Partial<Book>)
-      .subscribe((response) => {
-        console.log('la respuesta', response);
-      });
+    this.formSubmit.emit(this.bookData.value as Partial<Book>);
   }
 }
--- src/app/pages/createbook/createbook.component.ts
+++ src/app/pages/createbook/createbook.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MainContainerComponent } from '../../components/layout/main-container/main-container.component';
 import { FormComponent } from '../../components/form/form.component';
+import { BookService } from '../../core/services/book.service';
+import { Book } from '../../interface/book';
 
 @Component({
   selector: 'create-page',
@@ -8,4 +10,12 @@ import { FormComponent } from '../../components/form/form.component';
   imports: [MainContainerComponent, FormComponent],
   templateUrl: './createbook.component.html',
 })
-export class CreatebookComponent {}
+export class CreatebookComponent {
+  constructor(private bookService: BookService) {}
+
+  sendForm(bookData: Partial<Book>) {
+    this.bookService.create(bookData).subscribe((response) => {
+      console.log('la respuesta', response);
+    });
+  }
+}
