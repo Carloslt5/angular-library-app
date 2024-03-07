07ddf860d191b46529ffe11d00a15114ce825856
Carloslt <carloslt.proyectos@gmail.com>
2024-03-07 20:51:36 +0100

    ✅ task: add close toast

--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -1,22 +1,33 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ToastService } from '../../core/services/toast.service';
 import { SuccessResponseAPI } from '../../interface/http-response';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'toast',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './toast.component.html',
 })
 export class ToastComponent implements OnInit {
-  @Input() notification!: SuccessResponseAPI;
+  @Input() notification: SuccessResponseAPI | null = null;
 
   constructor(private toastServices: ToastService) {}
   ngOnInit() {
+    this.initToast();
+  }
+
+  initToast() {
     this.toastServices.notifications$.subscribe((res) => {
-      if ('status' in res) {
+      if (res && 'status' in res) {
         this.notification = res;
+      } else {
+        this.notification = null;
       }
     });
+  }
+
+  closeToast() {
+    this.toastServices.close();
   }
 }
--- src/app/core/services/toast.service.ts
+++ src/app/core/services/toast.service.ts
@@ -6,10 +6,13 @@ import { APIResponse } from '../../interface/http-response';
   providedIn: 'root',
 })
 export class ToastService {
-  private notificationsSubject = new Subject<APIResponse>();
+  private notificationsSubject = new Subject<APIResponse | null>();
   notifications$ = this.notificationsSubject.asObservable();
 
   add(responseData: APIResponse) {
     this.notificationsSubject.next(responseData);
+  }
+  close() {
+    this.notificationsSubject.next(null);
   }
 }
