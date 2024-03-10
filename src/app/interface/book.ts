e534ea075b50605a9d1d318493a5dc8a52916527
Carloslt <carloslt.proyectos@gmail.com>
2024-03-10 11:46:52 +0100

    🔧 refactor: add enum bookcategories

--- src/app/interface/book.ts
+++ src/app/interface/book.ts
@@ -1,22 +1,24 @@
-export type BookCategories =
-  | 'Adventure'
-  | 'Biography'
-  | 'Classic'
-  | 'Drama'
-  | 'Dystopian'
-  | 'Fantasy'
-  | 'Fiction'
-  | 'Historical Fiction'
-  | 'Historical'
-  | 'Horror'
-  | 'Humor'
-  | 'Mystery'
-  | 'Non-fiction'
-  | 'Poetry'
-  | 'Psychological Suspense'
-  | 'Romance'
-  | 'Science Fiction'
-  | 'Thriller';
+export enum BookCategories {
+  Adventure = 'Adventure',
+  Biography = 'Biography',
+  Classic = 'Classic',
+  Drama = 'Drama',
+  Dystopian = 'Dystopian',
+  Fantasy = 'Fantasy',
+  Fiction = 'Fiction',
+  HistoricalFiction = 'Historical Fiction',
+  Historical = 'Historical',
+  Horror = 'Horror',
+  Humor = 'Humor',
+  Mystery = 'Mystery',
+  NonFiction = 'Non-fiction',
+  Poetry = 'Poetry',
+  PsychologicalSuspense = 'Psychological Suspense',
+  Romance = 'Romance',
+  ScienceFiction = 'Science Fiction',
+  Thriller = 'Thriller',
+}
+
 export interface Book {
   id: string;
   author: string;
