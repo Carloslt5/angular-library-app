a6687a18938b02eb525860f48892ec752ee81034
Carloslt <carloslt.proyectos@gmail.com>
2024-03-11 19:57:03 +0100

    ✅ task: feature menu mobile responsive

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,23 @@
-import { NgOptimizedImage } from '@angular/common';
+import { NgClass, NgOptimizedImage } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'navbar-component',
   standalone: true,
-  imports: [NgOptimizedImage, RouterLink],
+  imports: [NgClass, NgOptimizedImage, RouterLink],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
   navBarLogo = '/assets/library-icon.svg';
+  menuMobileVisible = false;
   @Input() titleApp!: string;
+
+  showMenuMobile() {
+    this.menuMobileVisible = !this.menuMobileVisible;
+  }
+
+  closeMenu() {
+    this.menuMobileVisible = false;
+  }
 }
